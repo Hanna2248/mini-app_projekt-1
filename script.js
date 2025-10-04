6772a5024224c0ad5388e65a93b4da247beabffb
melamed <melameddavid19@gmail.com>
2025-10-04 11:12:36 +0200

    Add HTML button without id and JS function to add new categories. Function doesn't contain ID for btn because not existing yet.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function showAllImages(userName) {
-    console.log("Hallo, meine liebe " + userName)
-}
+function addNewCategory() {
+    const categoriesDiv = document.getElementById('categories');
+    const addButton = document.getElementById(); // how to access the button?
 
-function showDate() {
-    console.log(Date())
+    // functionality to add new button
+    const newButton = document.createElement('button');
+    newButton.innerHTML = 'new Category';
+    newButton.setAttribute('data-filter', 'new Category');
+    categoriesDiv.insertBefore(newButton, addButton);
+
+    console.log("Ich wurde gecklickt!")
 }
