d2d72f7b0143bcc3733c9ec0783774f7232309c6
melamed <melameddavid19@gmail.com>
2025-10-04 15:46:47 +0200

    Adjust addNewCategory by removing unused HTML attributes

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function addNewCategory() {
     // functionality to add new button
     const newButton = document.createElement('button');
     newButton.innerHTML = 'new Category';
-    newButton.setAttribute('data-filter', 'new Category');
     categoriesDiv.insertBefore(newButton, addButton);
 
     console.log("Ich wurde gecklickt!")
