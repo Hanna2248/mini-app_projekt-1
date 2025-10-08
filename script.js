9d1d7f298faf1a7f5cd927a122791da4789bf05a
Hanna Trush <annetta2248@gmail.com>
2025-10-08 14:52:18 +0200

    button11

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-const addBtn = document.getElementById('addBtn');
-const categoriesDiv = document.getElementById('categories');
-const gallery = document.getElementById('gallery');
+const gerBtn = document.getElementById('Deutschland')
+const amsBtn = document.getElementById('Amsterdam')
 
-let categoryCount = 0;
+const filterButtons = document.querySelectorAll(".filter-btn");
+const images = document.querySelectorAll(".gallery img");
 
-// когда нажимаем "+"
-addBtn.addEventListener('click', () => {
-  categoryCount++;
-
-  // создаём новую кнопку категории
-  const newCategory = document.createElement('button');
-  newCategory.textContent = `Категория ${categoryCount}`;
-  newCategory.classList.add('category-btn');
-
-  // добавляем кнопку перед плюсом
-  categoriesDiv.insertBefore(newCategory, addBtn);
-
-  // когда нажимаем на новую категорию — показываем её контент
-  newCategory.addEventListener('click', () => {
-    gallery.innerHTML = `<p>Вы открыли ${newCategory.textContent}. Здесь могут быть её картинки.</p>`;
-  });
-});
-    console.log("Ich wurde gecklickt!")
-}
+console.log(
+    gerBtn.innerHTML,
+    )
