fcf432ad7b1e38fc380399284d33a05d608ef555
Hanna Trush <annetta2248@gmail.com>
2025-10-08 16:22:30 +0200

    button111

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const amsBtn = document.getElementById('Amsterdam')
 const filterButtons = document.querySelectorAll(".filter-btn");
 const images = document.querySelectorAll(".gallery img");
 
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    // Убираем класс active у всех кнопок
+    filterButtons.forEach(btn => btn.classList.remove("active"));
+    // Добавляем active к текущей кнопке
+    button.classList.add("active");
+
+const category = button.getAttribute("data-category");
+
+    images.forEach(img => {
+      const imgCategory = img.getAttribute("data-category").split("");
+
+      // Если выбрана "all" или категория совпадает — показываем
+      if (category === "all" || category === imgCategory) {
+        img.classList.remove("hidden");
+      } else {
+        img.classList.add("hidden");
+      }
+    });
+  });
+});
+
 console.log(
     gerBtn.innerHTML,
     )
